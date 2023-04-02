1b9c127ae5b5d9c4b9efe3a8519d4c0be25011db
Haroldluengas7 <hluengas@uniempresarial.edu.co>
2023-04-02 15:59:36 -0500

    cambios

--- dist/formulario.js
+++ dist/formulario.js
@@ -38,15 +38,15 @@ const validarFormulario = (e) => {
 
 const validarCampo = (expresion, input, campo) => {
 	if(expresion.test(input.value)){
-		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+		document.getElementById(`grupo__${campo}`).classList.remove('formularioTeam-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.add('formularioTeam-correcto');
 		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
 		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
 		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
 		campos[campo] = true;
 	} else {
-		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+		document.getElementById(`grupo__${campo}`).classList.add('formularioTeam-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.remove('formularioTeam-correcto');
 		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
 		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
 		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
@@ -72,7 +72,7 @@ formulario.addEventListener('submit', (e) => {
 			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
 		}, 5000);
 
-		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+		document.querySelectorAll('.formularioTeam-correcto').forEach((icono) => {
 			icono.classList.remove('formulario__grupo-correcto');
 		});
 	} else {
